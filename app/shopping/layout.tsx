7915c0088ba90d96c675f0dd8e15a8f768480c88
aaku99 <aakritihamal99@gmail.com>
2026-02-28 21:11:36 -0500

    shopping

--- app/shopping/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SiteLayout from "@/app/components/SiteLayout";
-
-export default function ShoppingLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SiteLayout>{children}</SiteLayout>;
-}
